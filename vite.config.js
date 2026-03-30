aef33c706263d7670db9777493605d851d180d86
Nicolas Germeau <ngermeau@pm.me>
2026-03-30 14:42:10 +0200

    Setup custom domain nicolasgermeau.com

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nicolasgermeau.com/",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
